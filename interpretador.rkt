0c56f5532059ab00852c185870a42d4e4bad6e44
sebaspapu <sebasnarutoxd@gmail.com>
2026-05-23 17:21:27 -0500

    feat: initializing repo

---
+++ interpretador.rkt
@@ -0,0 +1,10 @@
+#lang eopl
+#|
+Integrantes:
+- Jairo Hernan Gonzalez Barreto - 202324314
+- Sebastian Bolaños Morales -202310168
+
+Repositorio GitHub:
+ https://github.com/sebaspapu/FLP-taller3
+|#
+
