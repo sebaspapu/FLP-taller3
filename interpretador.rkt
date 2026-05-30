ab5d0106d9e4bab926c239d8da29e21a33dcedc2
Jairo <gonzalez.jairo@correounivalle.edu.co>
2026-05-29 21:34:04 -0500

    Version final

--- interpretador.rkt
+++ interpretador.rkt
@@ -42,10 +42,12 @@ Repositorio GitHub:
     ; Válidos: @x  @suma  @mi_var
     (identificador-token ("@" letter (arbno (or letter digit "_"))) symbol)
  
-    ; Texto entre comillas dobles
+    ; texto-token captura solo el contenido entre comillas
+    ; las comillas son parte del patrón léxico (delimitadores),
+    ; el valor almacenado en el token es solo el texto interior
     (texto-token
-    (letter (arbno (or letter digit "_" ":" "!" "?" "." "," "-")))
-    string)
+     ("\"" (arbno (or letter digit "_" ":" "!" "?" "." "," "-" " ")) "\"")
+     string)
     
   ))
 
@@ -69,9 +71,9 @@ Repositorio GitHub:
  
     ; Texto literal entre comillas
     (<expression>
-     ("\"" texto-token "\"")
+     (texto-token)
      texto-lit)
- 
+
     ; VARIABLE: identificador que empieza con @
     (<expression>
      (identificador-token)
@@ -365,8 +367,8 @@ Repositorio GitHub:
       ; Número literal: retorna el número directamente
       (numero-lit (n) n)
 
-      ; Texto literal: retorna el string directamente
-      (texto-lit (t) t)
+      ;  Texto literal: quita las comillas que el lexer incluye en el valor
+      (texto-lit (t)(substring t 1 (- (string-length t) 1)))
 
       ; Variable: busca en el ambiente
       (var-exp (id)
@@ -617,4 +619,26 @@ declarar (
 ; evaluar @decorate("_ProfesoresFLP") finEval
 ; debe retornar: "Hola:Jairo_y_Sebastian_EstudiantesFLP"
 
+#|
+
+(interpretar
+"
+declarar (
+  @integrantes = procedimiento () {
+    \"Jairo_y_Sebastian\"
+  };
+
+  @saludar = procedimiento (@f) {
+    procedimiento (@msg) {
+      ((\"Hola:\" concat evaluar @f() finEval) concat @msg)
+    }
+  };
+
+  @decorate = evaluar @saludar(@integrantes) finEval;
+) {
+  evaluar @decorate(\"_EstudiantesFLP\") finEval
+}
+")
+
+|#
 
